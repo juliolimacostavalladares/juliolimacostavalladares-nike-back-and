13cd6dce706666996051e3e1a8c68400f3b35285
Julio Valladares <juliolima1225@gamil.com>
2021-03-14 17:48:58 -0300

    deploy on Heroku

--- src/main.js
+++ src/main.js
@@ -1,32 +1,44 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const url = 'https://www.nike.com.br/snkrs#calendariol';
-const selector = '.snkr-release.produto.produto--aviseme';
-(async function(){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0); 
-    await page.goto(url);
-    const recipes = await page.$$eval(selector, nodes => {
-        return nodes.map(node => {
-            const img = node.querySelector('img').getAttribute('data-src');
-            const day = node.querySelector('.snkr-release__day').textContent;
-            const month = node.querySelector('.snkr-release__month').textContent;
-            const hour = node.querySelector('.snkr-release__mobile-date').textContent;
-            const name = node.querySelector('.snkr-release__name').textContent;
-            const arry = {
-                name: name,
-                data: {
-                    day,
-                    month,
-                    hour
-                },
-                img
-        }
-            return arry
+const express = require('express'); // Adding Express
+    const app = express(); // Initializing Express
+    const puppeteer = require('puppeteer'); // Adding Puppeteer
+
+    const url = 'https://www.nike.com.br/snkrs#calendariol';
+    const selector = '.snkr-release.produto.produto--aviseme';
+
+    // Wrapping the Puppeteer browser logic in a GET request
+    app.get('/', function(req, res) {
+
+        // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
+        puppeteer.launch().then(async function(browser) {
+            const page = await browser.newPage();
+            await page.setDefaultNavigationTimeout(0);  
+            await page.goto(url);
+            const recipes = await page.$$eval(selector, nodes => {
+                return nodes.map(node => {
+                    const img = node.querySelector('img').getAttribute('data-src');
+                    const day = node.querySelector('.snkr-release__day').textContent;
+                    const month = node.querySelector('.snkr-release__month').textContent;
+                    const hour = node.querySelector('.snkr-release__mobile-date').textContent;
+                    const name = node.querySelector('.snkr-release__name').textContent;
+                    const arry = {
+                        name: name,
+                        data: {
+                            day,
+                            month,
+                            hour
+                        },
+                        img
+                }
+                return arry
         })
-    });;
-    console.log(recipes)
-    fs.writeFile('./db.json', JSON.stringify(recipes, null, 2), err => err ? console.log(err): null);
-    await browser.close();
-})();
+
+    });
+            // Closing the Puppeteer controlled headless browser
+            await browser.close();
+            // Sending the Digimon names to Postman
+            res.send(recipes);
+        });
+    });
+
+    // Making Express listen on port 7000
+    app.listen(process.env.PORT || 3333);
